56233ecec19d0ae6dd5641cf5762b2f90afad9c9
Shubhangi Vajpai <106021469+shubhangivajpai@users.noreply.github.com>
2023-10-09 23:21:34 +0530

    Add files via upload

---
+++ bubble sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(arr):
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+arr=list(map(int,input().split()))
+n=len(arr)
+print(bubble_sort(arr))

f02045372baf932773892d904894d8752bf67900
Shubhangi Vajpai <106021469+shubhangivajpai@users.noreply.github.com>
2023-10-30 23:31:15 +0530

    Add files via upload

---
+++ trees/root_equal_sum_of_children.py
@@ -0,0 +1,15 @@
+# Root equal sum of children
+#Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        if root==None:
+            return 0
+        if root.left.val+root.right.val==root.val:
+            return True
+        else:
+            return False
